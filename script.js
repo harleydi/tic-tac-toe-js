2b238d0df3970d5cfd8ac92abce054dee7f2da45
harleydi <disneyharley26@gmail.com>
2019-12-27 13:31:20 -0500

    completed board layout

---
+++ script.js
@@ -0,0 +1,11 @@
+//Create a board
+let board = document.querySelector('#board');
+let player1 =
+let player2 = 
+console.log(board);
+//Users should be able to click square and see "X" or "O"
+//Every click will alternate between being for Player 1 or Player 2. The visual feedback should indicate whose turn it is.
+//A cell should not be able to be replayed once marked.
+//Add a reset button that will clear the contents of the board.
+//After the necessary moves have been played, stop game and alert the player to a win, loss or stalemate.
+    //Hint: Determine a set of winning combinations. Check those combinations on the board contents after every move.
